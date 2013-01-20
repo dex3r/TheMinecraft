44fc4041217824606a52ed33af1417f3626a0cb1
DeX3r <dex3r.dx@gmail.com>
2013-01-20 12:38:47 +0100

    testt2

--- minecraft/dex3r/main/factions/Faction.java
+++ minecraft/dex3r/main/factions/Faction.java
@@ -23,7 +23,7 @@ public class Faction
 	public Faction(String owner)
 	{
 		this.owner = owner;
-		lvl = 1;
+		lvl = 666;
 		maxMembers = 777;
 		members = new ArrayList<FactionMember>();
 		members.add(new FactionMember(owner, FactionMemberRank.Owner));
